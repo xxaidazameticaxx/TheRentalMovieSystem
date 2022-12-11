0ef7f7b4e842224f03255cfc7086e57cf095dabd
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 18:13:21 +0100

    created connection with database in UsersDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
@@ -2,9 +2,21 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Users;
 
+
 import java.util.List;
+import java.sql.*;
 
 public class UsersDaoSQLImpl implements UsersDao{
+    private Connection conn;
+
+    public UsersDaoSQLImpl(){
+        try{
+            this.conn = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/root","root","root");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Users getById(int id) {
         return null;
