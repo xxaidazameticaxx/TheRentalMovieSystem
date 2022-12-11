ddc32cff7436f9b1af7bdc7489f6e943304309aa
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 20:47:49 +0100

    misplaced getUserByRentId method and getMovieByRentId

--- src/main/java/ba/unsa/etf/rpr/dao/RentsDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RentsDaoSQLImpl.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Movies;
 import ba.unsa.etf.rpr.domain.Rents;
 import ba.unsa.etf.rpr.domain.Users;
 
@@ -17,6 +18,60 @@ public class RentsDaoSQLImpl implements RentsDao{
         }
     }
 
+    public Movies getMovieByRentId(int rent_id) {
+        String query = "SELECT * FROM movies WHERE movie_id = (SELECT r.movie_id FROM rents r WHERE r.id = ?)";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(query);
+            stmt.setInt(1, rent_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){ // result set is iterator.
+                Movies movie = new Movies();
+                movie.setMovie_id(rs.getInt("movie_id"));
+                movie.setMovie_name(rs.getString("movie_name"));
+                movie.setPrice(rs.getDouble("price"));
+                movie.setGenre(rs.getString("genre"));
+                movie.setDuration(rs.getInt("duration"));
+                movie.setRatings(rs.getDouble("ratings"));
+                movie.setRelease_date(rs.getDate("release_date"));
+                movie.setLanguage(rs.getString("language"));
+                rs.close();
+                return movie;
+            }
+            else{
+                return null; // if there is no elements in the result set return null
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return null;
+    }
+
+    public Users getUserByRentId(int rent_id) {
+        String query = "SELECT * FROM users WHERE user_id = (SELECT r.user_id FROM rents r WHERE r.id = ?)";
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(query);
+            stmt.setInt(1, rent_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){ // result set is iterator.
+                Users user = new Users();
+                user.setUser_id(rs.getInt("user_id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setAdmin(rs.getBoolean("admin"));
+                rs.close();
+                return user;
+            }
+            else{
+                return null; // if there is no elements in the result set return null
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace(); // poor error handling
+        }
+        return null;
+    }
+
     @Override
     public Rents getById(int rent_id) {
         String query = "SELECT * FROM rents WHERE rent_id = ?";
@@ -27,14 +82,8 @@ public class RentsDaoSQLImpl implements RentsDao{
             if (rs.next()){ // result set is iterator.
                 Rents rent = new Rents();
                 rent.setRent_id(rs.getInt("rent_id"));
-                /*
-                rent.setMovie(new MoviesDaoSQLImpl().getById(rs.getInt("movie_id")));
-                rent.setUser(new UsersDaoSQLImpl().getById(rs.getInt("user_id")));
-
-                rent.setMovie(getMovieByRentId(rent_id));
-                rent.setUser(getUserByRentId(rent_id));
-                */
-
+                rent.setMovie(getMovieByRentId(rent_id)); //rent.setMovie(new MoviesDaoSQLImpl().getById(rs.getInt("movie_id")));
+                rent.setUser(getUserByRentId(rent_id));   //rent.setUser(new UsersDaoSQLImpl().getById(rs.getInt("user_id")));
                 rent.setRent_date(rs.getDate("rent_date"));
                 rent.setReturn_date(rs.getDate("return_date"));
                 rs.close();
