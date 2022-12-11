155c64ee5d2b5fdd9d44a073bea415f58c8bce77
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 18:08:52 +0100

    created connection with database in MoviesDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/MoviesDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/MoviesDaoSQLImpl.java
@@ -2,9 +2,21 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Movies;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 public class MoviesDaoSQLImpl implements MoviesDao{
+
+    private Connection conn;
+
+    public MoviesDaoSQLImpl(){
+        try{
+            this.conn = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/root","root","root");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     @Override
     public Movies getById(int id) {
         return null;
