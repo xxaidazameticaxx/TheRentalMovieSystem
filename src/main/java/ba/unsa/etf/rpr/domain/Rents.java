5769646a219dfd2b2ce765bd78f4599ce73c2825
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 16:40:21 +0100

    implemented Rents.java class with attributes, setters and getters

--- src/main/java/ba/unsa/etf/rpr/domain/Rents.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Rents.java
@@ -1,4 +1,54 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Rents {
+    private int rent_id;
+    private int movie_id;
+    private int user_id;
+    private Date rent_date;
+    private Date return_date;
+
+    public int getRent_id() {
+        return rent_id;
+    }
+
+    public void setRent_id(int rent_id) {
+        this.rent_id = rent_id;
+    }
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(int movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public Date getRent_date() {
+        return rent_date;
+    }
+
+    public void setRent_date(Date rent_date) {
+        this.rent_date = rent_date;
+    }
+
+    public Date getReturn_date() {
+        return return_date;
+    }
+
+    public void setReturn_date(Date return_date) {
+        this.return_date = return_date;
+    }
+
+
 }
