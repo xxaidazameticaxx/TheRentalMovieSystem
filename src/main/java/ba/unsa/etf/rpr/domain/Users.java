e3e340d80c88039e426cb69ba0d8e4c6adc9cdb1
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 16:47:26 +0100

    added attributes, setters and getters to Users.java

--- src/main/java/ba/unsa/etf/rpr/domain/Users.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Users.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
 public class Users {
+    private int user_id;
+    private String username;
+    private String password;
+    private boolean admin;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
 }
