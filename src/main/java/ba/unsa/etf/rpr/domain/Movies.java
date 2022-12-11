19a1d2475d48e4de5ebe370912048111784933a4
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-11 16:52:42 +0100

    added attributes, setters and getters in Movies.java

--- src/main/java/ba/unsa/etf/rpr/domain/Movies.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Movies.java
@@ -1,4 +1,80 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Movies {
+    private int movie_id;
+    private String movie_name;
+    private double price;
+    private String genre;
+    private int duration;
+    private double ratings;
+    private Date release_date;
+    private String language;
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(int movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public String getMovie_name() {
+        return movie_name;
+    }
+
+    public void setMovie_name(String movie_name) {
+        this.movie_name = movie_name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(double ratings) {
+        this.ratings = ratings;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
 }
