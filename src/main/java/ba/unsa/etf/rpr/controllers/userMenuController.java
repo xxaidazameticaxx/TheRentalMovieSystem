2a6ef48114a070d4045df0c73ee5ac8674d410a4
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-21 21:27:38 +0100

    addes attributes and some methods in userMenuController.java

--- src/main/java/ba/unsa/etf/rpr/controllers/userMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/userMenuController.java
@@ -1,14 +1,23 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 
 public class userMenuController {
     public TextField welcomeTextField_id;
+    public Button myMoviesButton_id;
+    public Button allMoviesButton_id;
 
     public void setWelcomeTextField_id(String welcomeUserText) {
+
         welcomeTextField_id.setText(welcomeUserText);
     }
 
+    public void allMoviesClick(ActionEvent actionEvent) {
+    }
 
+    public void myMoviesClick(ActionEvent actionEvent) {
+    }
 }
