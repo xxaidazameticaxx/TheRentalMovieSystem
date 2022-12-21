98a5cc2d141ec9896a231b3836bf68339851db68
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-21 12:52:02 +0100

    added Buton id's

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 
 public class SampleController {
+    public Button signupButton_id;
+    public Button loginButton_id;
+
     public void loginclick(ActionEvent actionEvent) {
     }
 }
