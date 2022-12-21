aaa254bfb0ad0ad662493dfa4516485fcbbad859
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-21 23:40:33 +0100

    added button attributes

--- src/main/java/ba/unsa/etf/rpr/controllers/adminMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/adminMenuController.java
@@ -1,9 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class adminMenuController {
     public TextField welcomeTextField1_id;
+    public Button viewAllMoviesButton_id;
+    public Button viewRentedMoviesButton_id;
+    public Button viewAllUsersButton_id;
+    public Button addMovieButton_id;
+    public Button rentAMovieButton_id;
+    public Button returnMovieButton_id;
 
     public void setWelcomeTextField1_id(String welcomeAdminText) {
 
