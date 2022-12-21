742131a45cc83b0b86e320683953b89ba22ab23d
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-21 15:08:24 +0100

    changed colours in login.css and added trim() method for functionality in loginController

--- src/main/java/ba/unsa/etf/rpr/controllers/loginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/loginController.java
@@ -23,10 +23,11 @@ public class loginController {
     @FXML
     public void initialize() {
         usernameField_id.getStyleClass().add("poljeNijeIspravno");
+        passwordField_id.getStyleClass().add("poljeNijeIspravno");
         usernameField_id.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
-                if (usernameField_id.getText().isEmpty()) {
+                if (usernameField_id.getText().trim().isEmpty()) {
                     usernameField_id.getStyleClass().removeAll("fieldCorrect");
                     usernameField_id.getStyleClass().add("fieldNotCorrect");
                 } else {
@@ -35,13 +36,31 @@ public class loginController {
                 }
             }
         });
+
+        passwordField_id.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (passwordField_id.getText().trim().isEmpty()) {
+                    passwordField_id.getStyleClass().removeAll("fieldCorrect");
+                    passwordField_id.getStyleClass().add("fieldNotCorrect");
+                } else {
+                    passwordField_id.getStyleClass().removeAll("fieldNotCorrect");
+                    passwordField_id.getStyleClass().add("fieldCorrect");
+                }
+            }
+        });
     }
 
     public void loginclick(ActionEvent actionEvent) throws IOException {
-        if(usernameField_id.getText().trim().isEmpty()){
+        if(usernameField_id.getText().isEmpty()){
             usernameField_id.getStyleClass().add("fieldNotCorrect");
             return;
         }
+        if(passwordField_id.getText().isEmpty()){
+            passwordField_id.getStyleClass().add("fieldNotCorrect");
+            return;
+        }
+
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/userMenu.fxml"));
         stage.setTitle("UserMenu");
