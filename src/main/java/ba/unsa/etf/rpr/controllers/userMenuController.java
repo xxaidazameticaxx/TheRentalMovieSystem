e330d36cad0cde9c5e7546116e2588142afe1040
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-21 19:35:57 +0100

    passed the data from stage to scene for the welcome user text field

--- src/main/java/ba/unsa/etf/rpr/controllers/userMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/userMenuController.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.TextField;
+
+
 public class userMenuController {
+    public TextField welcomeTextField_id;
+
+    public void setWelcomeTextField_id(String welcomeUserText) {
+        welcomeTextField_id.setText(welcomeUserText);
+    }
+
+
 }
