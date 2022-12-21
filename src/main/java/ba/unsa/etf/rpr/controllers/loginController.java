c565c7251d79995832f3f3c6bf60ee1222d58342
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-22 00:42:26 +0100

    added signup.css and signup.fxml

--- src/main/java/ba/unsa/etf/rpr/controllers/loginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/loginController.java
@@ -93,4 +93,12 @@ public class loginController {
 
 
     }
+
+    public void signupClick(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/ba/unsa/etf/rpr/controllers/signupController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class signupController {
+    public Button signupButton1_id;
+    public PasswordField passwordField1_id;
+    public TextField usernameField1_id;
+
+    public void signupclick1(ActionEvent actionEvent) {
+    }
+}
