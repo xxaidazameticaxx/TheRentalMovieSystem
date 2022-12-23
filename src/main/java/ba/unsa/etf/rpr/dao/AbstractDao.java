674485cdebec4f6480ccdbd26d011b3e45260ef8
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-24 00:04:10 +0100

    implemented constructor in AbstractDao

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -2,10 +2,32 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
 
+import java.sql.*;
+import java.util.*;
+
 /**
  * Abstract class that implements core DAO CRUD methods for every entity
  *
  * @author Aida Zametica
  */
 public abstract class AbstractDao <T extends Idable>implements Dao<T> {
+    private Connection connection;
+    private String tableName;
+
+    public AbstractDao(String tableName){
+        try{
+            this.tableName = tableName;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("db.properties").openStream());
+            String url = p.getProperty("url");
+            String username = p.getProperty("user");
+            String password = p.getProperty("password");
+            this.connection = DriverManager.getConnection(url, username, password);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
 }
