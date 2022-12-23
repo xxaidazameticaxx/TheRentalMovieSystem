81ef095692cc923e16766dc81ccde6cfa31d4039
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-24 00:19:32 +0100

    created getById, getAll methods

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.MovieException;
 
 import java.sql.*;
 import java.util.*;
@@ -37,5 +38,29 @@ public abstract class AbstractDao <T extends Idable>implements Dao<T> {
     public void setConnection(Connection connection){
         this.connection = connection;
     }
+
+    /**
+     * Method for mapping ResultSet into Object
+     * @param rs - result set from database
+     * @return a Bean object for specific table
+     * @throws MovieException in case of error with db
+     */
+    public abstract T row2object(ResultSet rs) throws MovieException;
+
+    /**
+     * Method for mapping Object into Map
+     * @param object - a bean object for specific table
+     * @return key, value sorted map of object
+     */
+    public abstract Map<String, Object> object2row(T object);
+
+    public T getById(int id) throws MovieException {
+        return executeQueryUnique("SELECT * FROM "+this.tableName+" WHERE id = ?", new Object[]{id});
+    }
+
+    public List<T> getAll() throws MovieException {
+        return executeQuery("SELECT * FROM "+ tableName, null);
+    }
+
 
 }
