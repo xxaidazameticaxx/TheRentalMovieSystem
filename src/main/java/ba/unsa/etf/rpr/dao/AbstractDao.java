0b97ab9ba2bbb797fc2537b0905fd753da16945f
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-24 00:25:36 +0100

    created executeQuery method

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -129,5 +129,31 @@ public abstract class AbstractDao <T extends Idable>implements Dao<T> {
         }
     }
 
+    /**
+     * Utility method for executing any kind of query
+     * @param query - SQL query
+     * @param params - params for query
+     * @return List of objects from database
+     * @throws MovieException in case of error with db
+     */
+    public List<T> executeQuery(String query, Object[] params) throws MovieException{
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            if (params != null){
+                for(int i = 1; i <= params.length; i++){
+                    stmt.setObject(i, params[i-1]);
+                }
+            }
+            ResultSet rs = stmt.executeQuery();
+            ArrayList<T> resultList = new ArrayList<>();
+            while (rs.next()) {
+                resultList.add(row2object(rs));
+            }
+            return resultList;
+        } catch (SQLException e) {
+            throw new MovieException(e.getMessage(), e);
+        }
+    }
+
 
 }
