300ccc70f2303653439b7970598ffd98bfd58d16
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-24 22:47:49 +0100

    changed a button in adminMenu

--- src/main/java/ba/unsa/etf/rpr/controllers/adminMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/adminMenuController.java
@@ -7,11 +7,11 @@ import javafx.scene.control.TextField;
 public class adminMenuController {
     public TextField welcomeTextField1_id;
     public Button viewAllMoviesButton_id;
-    public Button viewRentedMoviesButton_id;
     public Button viewAllUsersButton_id;
     public Button addMovieButton_id;
     public Button rentAMovieButton_id;
     public Button returnMovieButton_id;
+    public Button viewAllRentsButton_id;
 
     public void setWelcomeTextField1_id(String welcomeAdminText) {
 
@@ -30,9 +30,10 @@ public class adminMenuController {
     public void clickViewAllUsers(ActionEvent actionEvent) {
     }
 
-    public void clickViewAllRentedMovies(ActionEvent actionEvent) {
-    }
 
     public void clickViewAllMovies(ActionEvent actionEvent) {
+    }
+
+    public void clickViewAllRents(ActionEvent actionEvent) {
     }
 }
