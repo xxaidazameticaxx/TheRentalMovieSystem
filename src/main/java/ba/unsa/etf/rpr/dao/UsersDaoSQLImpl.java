3e133982d5ca78b45ee656580ae1354730637928
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-24 01:54:05 +0100

    refactored UsersDaoSQLImpl

--- src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UsersDaoSQLImpl.java
@@ -1,127 +1,44 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Users;
+import ba.unsa.etf.rpr.exceptions.MovieException;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.sql.*;
-import java.util.Properties;
 
-public class UsersDaoSQLImpl implements UsersDao{
+public class UsersDaoSQLImpl extends AbstractDao<Users> implements UsersDao{
     private Connection conn;
 
     public UsersDaoSQLImpl(){
-        try{
-            FileReader fr = new FileReader("src/main/resources/db.properties");
-            Properties p = new Properties();
-            p.load(fr);
-            String url = p.getProperty("url");
-            String user = p.getProperty("user");
-            String password = p.getProperty("password");
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.conn = DriverManager.getConnection(url,user,password);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        super("USERS");
     }
 
     @Override
-    public Users getById(int id) {
-        String query = "SELECT * FROM USERS WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.conn.prepareStatement(query);
-            stmt.setInt(1, id);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()){ // result set is iterator.
-                Users user = new Users();
-                user.setId(rs.getInt("id"));
-                user.setUsername(rs.getString("username"));
-                user.setPassword(rs.getString("password"));
-                user.setAdmin(rs.getBoolean("admin"));
-                rs.close();
-                return user;
-            }
-            else{
-                return null; // if there is no elements in the result set return null
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace(); // poor error handling
-        }
-        return null;
-    }
-
-    @Override
-    public Users add(Users item) {
-        String insert = "INSERT INTO USERS(username,password,admin) VALUES(?,?,?)";
-        try{
-            PreparedStatement stmt = this.conn.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, item.getUsername());
-            stmt.setString(2, item.getPassword());
-            stmt.setBoolean(3, item.isAdmin());
-            stmt.executeUpdate();
-
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next(); // we know that there is one key
-            item.setId(rs.getInt(1)); //set id to return it back
-            return item;
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public Users update(Users item) {
-        String insert = "UPDATE USERS SET username = ?,password = ?, admin = ? WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.conn.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, item.getUsername());
-            stmt.setObject(2, item.getPassword());
-            stmt.setObject(3, item.isAdmin());
-            stmt.setObject(4, item.getId());
-            stmt.executeUpdate();
-            return item;
-        }catch (SQLException e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-    @Override
-    public void delete(int id) {
-        String insert = "DELETE FROM USERS WHERE id = ?";
-        try{
-            PreparedStatement stmt = this.conn.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
-            stmt.setObject(1, id);
-            stmt.executeUpdate();
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public List<Users> getAll() {
-        String query = "SELECT * FROM USERS";
+    public Users row2object(ResultSet rs) throws MovieException {
         try {
-            PreparedStatement stmt = this.conn.prepareStatement(query);
-            ResultSet rs = stmt.executeQuery();
-            ArrayList<Users> usersLista = new ArrayList<>();
-            while (rs.next()) {
-                Users user = new Users();
-                user.setId(rs.getInt("id"));
-                user.setUsername(rs.getString("username"));
-                user.setPassword(rs.getString("password"));
-                user.setAdmin(rs.getBoolean("admin"));
-                usersLista.add(user);
-            }
-            rs.close();
-            return usersLista;
-        } catch (SQLException e) {
-            e.printStackTrace();
+            Users user = new Users();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setAdmin(rs.getBoolean("admin"));
+            return user;
+        } catch (Exception e) {
+            throw new MovieException(e.getMessage(), e);
         }
-        return null;
     }
+
+    /**
+     * @param object - object to be mapped
+     * @return map representation of object
+     */
+    @Override
+    public Map<String, Object> object2row(Users object) {
+        Map<String, Object> item = new TreeMap<>();
+        item.put("id", object.getId());
+        item.put("username", object.getUsername());
+        item.put("password", object.getPassword());
+        item.put("admin", object.isAdmin());
+        return item;
+    }
+
 }
