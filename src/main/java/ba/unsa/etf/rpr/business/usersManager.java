8610d99ca93e2828ccb3eb7cf379a79b46fb8fbd
xxaidazameticaxx <azametica1@etf.unsa.ba>
2022-12-29 01:18:25 +0100

    implemented usersManager.java

--- src/main/java/ba/unsa/etf/rpr/business/usersManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/usersManager.java
@@ -1,8 +1,34 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Movies;
+import ba.unsa.etf.rpr.domain.Users;
+import ba.unsa.etf.rpr.exceptions.MovieException;
+
+import java.util.List;
+
 /**
  * Business Logic Layer for Users
  * @author Aida Zametica
  */
 public class usersManager {
+    public Users getById(int id) throws MovieException {
+        return DaoFactory.usersDao().getById(id);
+    }
+
+    public Users add(Users u) throws MovieException{
+        return DaoFactory.usersDao().add(u);
+    }
+
+    public void update(Users u) throws MovieException{
+        DaoFactory.usersDao().update(u);
+    }
+
+    void delete(int id) throws MovieException{
+        DaoFactory.usersDao().delete(id);
+    }
+
+    List<Users> getAll() throws MovieException{
+        return DaoFactory.usersDao().getAll();
+    }
 }
