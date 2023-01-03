449ba4a340958efd918fee63178ce9b2821538b6
xxaidazameticaxx <azametica1@etf.unsa.ba>
2023-01-03 20:33:49 +0100

    some improvements

--- src/main/java/ba/unsa/etf/rpr/controllers/allMoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/allMoviesController.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class allMoviesController {
+    public TextField searchButton_id;
+
+    public void searchButtonClick(ActionEvent actionEvent) {
+    }
 }
