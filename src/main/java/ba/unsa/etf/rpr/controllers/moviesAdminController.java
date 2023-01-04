eb3b4968080810cc9cb4664eda6aa25779a47f39
xxaidazameticaxx <azametica1@etf.unsa.ba>
2023-01-04 01:36:13 +0100

    implemented fxml for moviesAdmin

--- src/main/java/ba/unsa/etf/rpr/controllers/moviesAdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/moviesAdminController.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class moviesAdminController {
+    public TableView MovieTable_id;
+    public TextField searchButton_id;
+    public TextField release_year_id;
+    public TextField ratings_id;
+    public TextField language_id;
+    public TextField genre_id;
+    public TextField duration_id;
+    public Button addButton_id;
+    public TextField price_id;
+    public Button deleteButton_id;
+    public Button updateButton_id;
+
+    public void updateClick(ActionEvent actionEvent) {
+    }
+
+    public void deleteClick(ActionEvent actionEvent) {
+    }
+
+    public void addClick(ActionEvent actionEvent) {
+    }
+
+    public void searchButtonClick(ActionEvent actionEvent) {
+    }
 }
