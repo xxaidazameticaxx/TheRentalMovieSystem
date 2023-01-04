c8dd12d66179a70230db87a70dfb06f559265f86
xxaidazameticaxx <azametica1@etf.unsa.ba>
2023-01-04 01:44:46 +0100

    implemented fxml for usersAdmin

--- src/main/java/ba/unsa/etf/rpr/controllers/usersAdminController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/usersAdminController.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class usersAdminController {
+    public TextField searchButton_id;
+    public TextField username_id;
+    public TextField password_id;
+    public TextField admin_id;
+    public Button addButton_id;
+    public Button deleteButton_id;
+    public Button updateButton_id;
+    public TableView userTable_id;
+
+    public void searchButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void addClick(ActionEvent actionEvent) {
+    }
+
+    public void deleteClick(ActionEvent actionEvent) {
+    }
+
+    public void updateClick(ActionEvent actionEvent) {
+    }
 }
