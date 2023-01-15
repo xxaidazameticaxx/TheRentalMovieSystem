8b500b5f081d52ad504f8b23e4a141436ca55f25
xxaidazameticaxx <azametica1@etf.unsa.ba>
2023-01-15 22:46:23 +0100

    implemented fxml for usersAdmin

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -19,7 +19,7 @@ public abstract class AbstractDao <T extends Idable>implements Dao<T> {
         try{
             this.tableName = tableName;
             Properties p = new Properties();
-            p.load(ClassLoader.getSystemResource("db.properties").openStream());
+            p.load(ClassLoader.getSystemResource("db1.properties").openStream());
             String url = p.getProperty("url");
             String username = p.getProperty("user");
             String password = p.getProperty("password");
