e3c22f5bee8f43e599befa7a4008524132b62129
xxaidazameticaxx <115507855+xxaidazameticaxx@users.noreply.github.com>
2023-07-23 20:24:40 +0200

    Information user done

--- src/main/java/ba/unsa/etf/rpr/controllers/UserMenuController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserMenuController.java
@@ -8,7 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -64,5 +66,21 @@ public class UserMenuController {
         stage.setX(x);
         stage.setY(y);
         stage.show();
+    }
+
+    public void informationClick(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/userInformation.fxml")));
+            Stage stage = new Stage();
+            stage.getIcons().add(new Image("/img/icon.jpg") );
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void accountClick(MouseEvent mouseEvent) {
     }
 }
